d13ce9b54f37d4a14c26e180c8d2ba0eaa0c8693
Alex Ambrioso <aambrioso1@gmail.com>
2019-05-04 12:20:15 -0400

    Update TTT with ui.py

--- TTT with ui.py
+++ TTT with ui.py
@@ -1,4 +1,4 @@
-#  This program allows to players to play tictactoe.   It uses the GUI templates availale in Pythonista.  To run the program, you will need to copy the template file to the directory containing the program.
+#  This program allows to players to play tictactoe.   It uses the GUI templates availale in Pythonista.  To run the program, you will need to copy the template file, TicTacToe.pyui, to the directory containing the program.   You can find the file in this repository. 
 
 import ui
 
